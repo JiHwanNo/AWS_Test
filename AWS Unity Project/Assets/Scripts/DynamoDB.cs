7305e5809d1eb9b70604a3a38cd6090689a6ed71
JiHwanNo <83812895+JiHwanNo@users.noreply.github.com>
2022-01-05 17:25:33 +0900

    2022-01-05

---
+++ AWS Unity Project/Assets/Scripts/DynamoDB.cs
@@ -0,0 +1,338 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Amazon;
+using Amazon.CognitoIdentity;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+using System;
+using System.Collections.Generic;
+using Amazon.Runtime;
+using Amazon.DynamoDBv2.DocumentModel;
+
+public class DynamoDB : MonoBehaviour
+{
+    DynamoDBContext context;
+    static AmazonDynamoDBClient DBclient;
+    CognitoAWSCredentials credentials;
+    private void Awake()
+    {
+        credentials = new CognitoAWSCredentials("자신의 자격 증명 풀 ID", RegionEndpoint.APNortheast2);
+        DBclient = new AmazonDynamoDBClient(credentials, RegionEndpoint.APNortheast2);
+        context = new DynamoDBContext(DBclient);
+        
+    }
+
+    class HighLevelBatchWriteItem
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                DynamoDBContext context = new DynamoDBContext(DBclient);
+                SingleTableBatchWrite(context);
+                MultiTableBatchWrite(context);
+            }
+            catch (AmazonServiceException e) { Console.WriteLine(e.Message); }
+            catch (Exception e) { Console.WriteLine(e.Message); }
+
+            Console.WriteLine("To continue, press Enter");
+            Console.ReadLine();
+        }
+
+        private static void SingleTableBatchWrite(DynamoDBContext context)
+        {
+            Book book1 = new Book
+            {
+                Id = 902,
+                InPublication = true,
+                ISBN = "902-11-11-1111",
+                PageCount = "100",
+                Price = 10,
+                ProductCategory = "Book",
+                Title = "My book3 in batch write"
+            };
+            Book book2 = new Book
+            {
+                Id = 903,
+                InPublication = true,
+                ISBN = "903-11-11-1111",
+                PageCount = "200",
+                Price = 10,
+                ProductCategory = "Book",
+                Title = "My book4 in batch write"
+            };
+
+            var bookBatch = context.CreateBatchWrite<Book>();
+            bookBatch.AddPutItems(new List<Book> { book1, book2 });
+
+            Console.WriteLine("Performing batch write in SingleTableBatchWrite().");
+            bookBatch.Execute();
+        }
+
+        private static void MultiTableBatchWrite(DynamoDBContext context)
+        {
+            // 1. New Forum item.
+            Forum newForum = new Forum
+            {
+                Name = "Test BatchWrite Forum",
+                Threads = 0
+            };
+            var forumBatch = context.CreateBatchWrite<Forum>();
+            forumBatch.AddPutItem(newForum);
+
+            // 2. New Thread item.
+            Thread newThread = new Thread
+            {
+                ForumName = "S3 forum",
+                Subject = "My sample question",
+                KeywordTags = new List<string> { "S3", "Bucket" },
+                Message = "Message text"
+            };
+
+            DynamoDBOperationConfig config = new DynamoDBOperationConfig();
+            config.SkipVersionCheck = true;
+            var threadBatch = context.CreateBatchWrite<Thread>(config);
+            threadBatch.AddPutItem(newThread);
+            threadBatch.AddDeleteKey("some partition key value", "some sort key value");
+
+            var superBatch = new MultiTableBatchWrite(forumBatch, threadBatch);
+            Console.WriteLine("Performing batch write in MultiTableBatchWrite().");
+            superBatch.Execute();
+        }
+    }
+
+    [DynamoDBTable("Reply")]
+    public class Reply
+    {
+        [DynamoDBHashKey] //Partition key
+        public string Id
+        {
+            get; set;
+        }
+
+        [DynamoDBRangeKey] //Sort key
+        public DateTime ReplyDateTime
+        {
+            get; set;
+        }
+
+        // Properties included implicitly.
+        public string Message
+        {
+            get; set;
+        }
+        // Explicit property mapping with object persistence model attributes.
+        [DynamoDBProperty("LastPostedBy")]
+        public string PostedBy
+        {
+            get; set;
+        }
+        // Property to store version number for optimistic locking.
+        [DynamoDBVersion]
+        public int? Version
+        {
+            get; set;
+        }
+    }
+
+    [DynamoDBTable("Thread")]
+    public class Thread
+    {
+        // PK mapping.
+        [DynamoDBHashKey]      //Partition key
+        public string ForumName
+        {
+            get; set;
+        }
+        [DynamoDBRangeKey]      //Sort key
+        public String Subject
+        {
+            get; set;
+        }
+        // Implicit mapping.
+        public string Message
+        {
+            get; set;
+        }
+        public string LastPostedBy
+        {
+            get; set;
+        }
+        public int Views
+        {
+            get; set;
+        }
+        public int Replies
+        {
+            get; set;
+        }
+        public bool Answered
+        {
+            get; set;
+        }
+        public DateTime LastPostedDateTime
+        {
+            get; set;
+        }
+        // Explicit mapping (property and table attribute names are different.
+        [DynamoDBProperty("Tags")]
+        public List<string> KeywordTags
+        {
+            get; set;
+        }
+        // Property to store version number for optimistic locking.
+        [DynamoDBVersion]
+        public int? Version
+        {
+            get; set;
+        }
+    }
+
+    [DynamoDBTable("Forum")]
+    public class Forum
+    {
+        [DynamoDBHashKey]      //Partition key
+        public string Name
+        {
+            get; set;
+        }
+        // All the following properties are explicitly mapped,
+        // only to show how to provide mapping.
+        [DynamoDBProperty]
+        public int Threads
+        {
+            get; set;
+        }
+        [DynamoDBProperty]
+        public int Views
+        {
+            get; set;
+        }
+        [DynamoDBProperty]
+        public string LastPostBy
+        {
+            get; set;
+        }
+        [DynamoDBProperty]
+        public DateTime LastPostDateTime
+        {
+            get; set;
+        }
+        [DynamoDBProperty]
+        public int Messages
+        {
+            get; set;
+        }
+    }
+
+    [DynamoDBTable("ProductCatalog")]
+    public class Book
+    {
+        [DynamoDBHashKey] //Partition key
+        public int Id
+        {
+            get; set;
+        }
+        public string Title
+        {
+            get; set;
+        }
+        public string ISBN
+        {
+            get; set;
+        }
+        public int Price
+        {
+            get; set;
+        }
+        public string PageCount
+        {
+            get; set;
+        }
+        public string ProductCategory
+        {
+            get; set;
+        }
+        public bool InPublication
+        {
+            get; set;
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        try
+        {
+            DynamoDBContext context = new DynamoDBContext(DBclient);
+            // Get an item.
+            GetBook(context, 101);
+
+            // Sample forum and thread to test queries.
+            string forumName = "Amazon DynamoDB";
+            string threadSubject = "DynamoDB Thread 1";
+            // Sample queries.
+            FindRepliesInLast15Days(context, forumName, threadSubject);
+            FindRepliesPostedWithinTimePeriod(context, forumName, threadSubject);
+
+            // Scan table.
+            FindProductsPricedLessThanZero(context);
+            Console.WriteLine("To continue, press Enter");
+            Console.ReadLine();
+        }
+        catch (AmazonDynamoDBException e) { Console.WriteLine(e.Message); }
+        catch (AmazonServiceException e) { Console.WriteLine(e.Message); }
+        catch (Exception e) { Console.WriteLine(e.Message); }
+    }
+
+    private static void GetBook(DynamoDBContext context, int productId)
+    {
+        Book bookItem = context.Load<Book>(productId);
+
+        Console.WriteLine("\nGetBook: Printing result.....");
+        Console.WriteLine("Title: {0} \n No.Of threads:{1} \n No. of messages: {2}",
+                  bookItem.Title, bookItem.ISBN, bookItem.PageCount);
+    }
+
+    private static void FindRepliesInLast15Days(DynamoDBContext context,
+                            string forumName,
+                            string threadSubject)
+    {
+        string replyId = forumName + "#" + threadSubject;
+        DateTime twoWeeksAgoDate = DateTime.UtcNow - TimeSpan.FromDays(15);
+        IEnumerable<Reply> latestReplies =
+            context.Query<Reply>(replyId, QueryOperator.GreaterThan, twoWeeksAgoDate);
+        Console.WriteLine("\nFindRepliesInLast15Days: Printing result.....");
+        foreach (Reply r in latestReplies)
+            Console.WriteLine("{0}\t{1}\t{2}\t{3}", r.Id, r.PostedBy, r.Message, r.ReplyDateTime);
+    }
+
+    private static void FindRepliesPostedWithinTimePeriod(DynamoDBContext context,
+                                  string forumName,
+                                  string threadSubject)
+    {
+        string forumId = forumName + "#" + threadSubject;
+        Console.WriteLine("\nFindRepliesPostedWithinTimePeriod: Printing result.....");
+
+        DateTime startDate = DateTime.UtcNow - TimeSpan.FromDays(30);
+        DateTime endDate = DateTime.UtcNow - TimeSpan.FromDays(1);
+
+        IEnumerable<Reply> repliesInAPeriod = context.Query<Reply>(forumId,
+                                       QueryOperator.Between, startDate, endDate);
+        foreach (Reply r in repliesInAPeriod)
+            Console.WriteLine("{0}\t{1}\t{2}\t{3}", r.Id, r.PostedBy, r.Message, r.ReplyDateTime);
+    }
+
+    private static void FindProductsPricedLessThanZero(DynamoDBContext context)
+    {
+        int price = 0;
+        IEnumerable<Book> itemsWithWrongPrice = context.Scan<Book>(
+            new ScanCondition("Price", ScanOperator.LessThan, price),
+            new ScanCondition("ProductCategory", ScanOperator.Equal, "Book")
+            );
+        Console.WriteLine("\nFindProductsPricedLessThanZero: Printing result.....");
+        foreach (Book r in itemsWithWrongPrice)
+            Console.WriteLine("{0}\t{1}\t{2}\t{3}", r.Id, r.Title, r.Price, r.ISBN);
+    }
+
+    
+}
