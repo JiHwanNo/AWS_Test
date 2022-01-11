10a7148969b89643ed611c3e718b274de8c46f94
JiHwanNo <83812895+JiHwanNo@users.noreply.github.com>
2022-01-11 17:30:52 +0900

    2022-01-11

---
+++ AWS Unity Project/Assets/Scripts/ApiManager.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class ApiManager : MonoBehaviour
+{
+    private const string Api = "https://qqz3a5ix7b.execute-api.ap-northeast-2.amazonaws.com/AWStest";
+    private Login_Test _authenticationManager;
+
+    public async void CallTestApi()
+    {
+        UnityWebRequest webRequest = UnityWebRequest.Get(Api);
+
+        // "Include the identity token in the Authorization header... "
+        // Doesn't seem to need the 'Bearer' term in front of the token... IT'S A BEAR DAAAAAAAANCE!!!!
+        // https://docs.aws.amazon.com/apigateway/latest/developerguide/apigateway-invoke-api-integrated-with-cognito-user-pool.html
+        webRequest.SetRequestHeader("Authorization", _authenticationManager.GetIdToken());
+
+        await webRequest.SendWebRequest();
+
+        if (webRequest.result != UnityWebRequest.Result.Success)
+        {
+            Debug.Log("API call failed: " + webRequest.error + "\n" + webRequest.result + "\n" + webRequest.responseCode);
+        }
+        else
+        {
+            Debug.Log("Success, API call complete!");
+            Debug.Log(webRequest.downloadHandler.text);
+        }
+        webRequest.Dispose();
+    }
+
+    void Awake()
+    {
+        _authenticationManager = FindObjectOfType<Login_Test>();
+    }
+}
--- AWS Unity Project/Assets/Scripts/Login_Test.cs
+++ AWS Unity Project/Assets/Scripts/Login_Test.cs
@@ -6,6 +6,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class Login_Test : MonoBehaviour
 {
@@ -18,7 +19,7 @@ public class Login_Test : MonoBehaviour
     private const string RedirectUrl = "https://www.google.com";
     private const string Region = "ap-northeast-2"; // Update with the AWS Region that contains your services
 
-    private const string AuthCodeGrantType = "authorization_code";
+    private const string AuthCodeGrantType  = "authorization_code";
     private const string RefreshTokenGrantType = "refresh_token";
     private const string CognitoAuthUrl = ".auth." + Region + ".amazoncognito.com";
     private const string TokenEndpointPath = "/oauth2/token";
@@ -34,9 +35,9 @@ public class Login_Test : MonoBehaviour
     void Start()
     {
         string loginUrl = GetLoginUrl();
-        Debug.LogError(loginUrl);
         Application.OpenURL(loginUrl);
     }
+
     public string GetLoginUrl()
     {
         // DOCS: https://docs.aws.amazon.com/cognito/latest/developerguide/login-endpoint.html
@@ -46,5 +47,85 @@ public class Login_Test : MonoBehaviour
         return loginUrl;
     }
 
+    public async void ProcessDeepLink(string deepLinkUrl)
+    {
+        // TODO: add some validation
 
+        // Debug.Log("UIInputManager.ProcessDeepLink: " + deepLinkUrl);
+        bool exchangeSuccess = await ExchangeAuthCodeForAccessToken(deepLinkUrl);
+
+    }
+    public async Task<bool> ExchangeAuthCodeForAccessToken(string rawUrlWithGrantCode)
+    {
+        // Debug.Log("rawUrlWithGrantCode: " + rawUrlWithGrantCode);
+
+        // raw url looks like https://somedomain.com/?code=c91d8bf4-1cb6-46e5-b43a-8def466f3c55
+        string allQueryParams = rawUrlWithGrantCode.Split('?')[1];
+
+        // it's likely there won't be more than one param
+        string[] paramsSplit = allQueryParams.Split('&');
+
+        foreach (string param in paramsSplit)
+        {
+            // Debug.Log("param: " + param);
+
+            // find the code parameter and its value
+            if (param.StartsWith("code"))
+            {
+                string grantCode = param.Split('=')[1];
+                string grantCodeCleaned = grantCode.removeAllNonAlphanumericCharsExceptDashes(); // sometimes the url has a # at the end of the string
+                return await CallCodeExchangeEndpoint(grantCodeCleaned);
+            }
+            else
+            {
+                Debug.Log("Code not found");
+            }
+        }
+        return false;
+    }
+
+
+    private static string _userid = "";
+    private async Task<bool> CallCodeExchangeEndpoint(string grantCode)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("grant_type", AuthCodeGrantType);
+        form.AddField("client_id", AppClientID);
+        form.AddField("code", grantCode);
+        form.AddField("redirect_uri", RedirectUrl);
+
+        // DOCS: https://docs.aws.amazon.com/cognito/latest/developerguide/token-endpoint.html
+        string requestPath = "https://" + AuthCognitoDomainPrefix + CognitoAuthUrl + TokenEndpointPath;
+
+        UnityWebRequest webRequest = UnityWebRequest.Post(requestPath, form);
+        await webRequest.SendWebRequest();
+
+        if (webRequest.result != UnityWebRequest.Result.Success)
+        {
+            Debug.Log("Code exchange failed: " + webRequest.error + "\n" + webRequest.result + "\n" + webRequest.responseCode);
+            webRequest.Dispose();
+        }
+        else
+        {
+            Debug.Log("Success, Code exchange complete!");
+
+            BADAuthenticationResultType authenticationResultType = JsonUtility.FromJson<BADAuthenticationResultType>(webRequest.downloadHandler.text);
+            // Debug.Log("ID token: " + authenticationResultType.id_token);
+
+            _userid = AuthUtilities.GetUserSubFromIdToken(authenticationResultType.id_token);
+
+            // update session cache
+            SaveDataManager.SaveJsonData(new UserSessionCache(authenticationResultType, _userid));
+            webRequest.Dispose();
+            return true;
+        }
+        return false;
+    }
+
+    public string GetIdToken()
+    {
+        UserSessionCache userSessionCache = new UserSessionCache();
+        SaveDataManager.LoadJsonData(userSessionCache);
+        return userSessionCache.getIdToken();
+    }
 }
